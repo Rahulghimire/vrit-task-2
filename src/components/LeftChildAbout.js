f87df0bfea7408e574d34b3babf7d8f4b2f1077d
Rahul Ghimire <ghimirerahul554@gmail.com>
2024-04-24 18:03:49 +0545

    chnages to banner left

--- src/components/LeftChildAbout.js
+++ src/components/LeftChildAbout.js
@@ -5,14 +5,14 @@ const LeftChild = () => {
     <div className="flex flex-col gap-y-[6px]">
       <div className="flex items-center gap-x-2">
         <div className="w-[30px] h-[2px] bg-[#C2292E]"></div>
-        <div className="uppercase font-Merriweather text-[#C2292E] text-[16px] tracking-[8%] leading-[27px]">
+        <div className="uppercase font-Merriweather font-family-2 text-[#C2292E] text-[16px] tracking-[8%] leading-[27px]">
           About Himalayan fair reisen
         </div>
       </div>
       <div className="text-[#000] font-bold font-Merriweather text-[36px] capitalize leading-[1]">
         Best Trekking Partner For your holidays
       </div>
-      <p className="text-[#655C5C] font-[Work-Sans] text-[16px] leading-[25px]">
+      <p className="text-[#655C5C] font-[Work-Sans] text-[16px] leading-[25px] text-justify">
         At Himalayan Fair Reisen, safety is paramount. Our guides are trained in
         wilderness first aid and possess the expertise to navigate the rugged
         Himalayan terrain safely. Moreover, we prioritize responsible tourism, .
