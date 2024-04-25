af013b2ff0340373708250c924a5cd945e3d98e8
Rahul Ghimire <ghimirerahul554@gmail.com>
2024-04-25 09:05:25 +0545

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,18 @@ import footerImg from "../assets/footer-img.svg";
 
 const Footer = () => {
   return (
-      <div className="footer-container">
-        <img src={footerImg} 
-            className='image'
-        />
+    <div className="footer-container">
+      <img src={footerImg} className="image" />
 
-      <div className="p-[20px] xl:px-[100px] xl:pt-[35px]"></div>
-
+      <div className="p-[20px] xl:px-[100px] xl:pt-[35px]">
+        <div className="grid grid-cols-12">
+          <div className="col-span-5 text-[#fff]">1</div>
+          <div className="col-span-2">2</div>
+          <div className="col-span-2">3</div>
+          <div className="col-span-3">4</div>
+        </div>
       </div>
+    </div>
   );
 };
 
